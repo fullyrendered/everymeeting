4079dab7ac77ad0f6fd9ebb8f8296a4d2bc32543
woodyland <tomijaka70@yahoo.com>
2025-12-28 21:46:16 +0800

    Initial commit - fresh upload

---
+++ resources/views/layouts/layout2.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+   
+</head>
+<body>
+<<<<<<< Updated upstream
+    <nav>
+        <a href="/home">Home</a>
+=======
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <a href="/">Home</a>
+>>>>>>> Stashed changes
+        <a href="/about">About</a>
+    </nav>
+    
+    @yield('content')
+</body>
+</html>
